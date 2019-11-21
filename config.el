74b96b2b7bca6d49e5881e8d6b5e8958e9b0dc97
Dan LaManna <dan.lamanna@kitware.com>
2019-11-21 16:02:21 -0500

    initial commit

---
+++ config.el
@@ -0,0 +1,32 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+(after! magit
+  (setq magit-repository-directories '(( "~/p" . 4 )))
+
+  (transient-append-suffix 'magit-push '(0 -1)
+    '("+c" "Create merge-request"
+      "--push-option=merge_request.create"))
+
+  (transient-append-suffix 'magit-push '(0 -1)
+    '("+d" "Against master"
+      "--push-option=merge_request.target=master"))
+
+  (transient-append-suffix 'magit-push '(0 -1)
+    '("+m" "Merge on success"
+      "--push-option=merge_request.merge_when_pipeline_succeeds")))
+
+(after! company
+  (setq company-show-numbers t))
+
+;; (spacemacs/set-leader-keys "]y" '(lambda()
+;;                                    (interactive)
+;;                                    (counsel-projectile-rg "--type yaml")))
+;; (spacemacs/set-leader-keys "]p" '(lambda()
+;;                                    (interactive)
+;;                                    (counsel-projectile-rg "--type py")))
+
+
+(after! super-save
+  (setq super-save-auto-save-when-idle t)
+  (setq super-save-idle-duration 8)
+  (super-save-mode +1))
