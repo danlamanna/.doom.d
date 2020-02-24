0cd62db691f86045650420a683d7bc08dbd3a7e2
Dan LaManna <me@danlamanna.com>
2020-02-23 19:35:07 -0500

    Add various configuration changes

--- config.el
+++ config.el
@@ -54,18 +54,60 @@
       "--push-option=merge_request.merge_when_pipeline_succeeds")))
 
 (after! company
-  (setq company-show-numbers t))
+  (setq company-show-numbers t
+        company-minimum-prefix-length 1
+        company-idle-delay 0.0))
 
-;; (spacemacs/set-leader-keys "]y" '(lambda()
-;;                                    (interactive)
-;;                                    (counsel-projectile-rg "--type yaml")))
-;; (spacemacs/set-leader-keys "]p" '(lambda()
-;;                                    (interactive)
-;;                                    (counsel-projectile-rg "--type py")))
+(after! avy
+  (setq avy-all-windows t
+        avy-style 'pre))
 
+;; (use-package smooth-scrolling
+;;   :config (progn
+;;             (setq smooth-scroll-margin 20)
+;;             (smooth-scrolling-mode 1)))
+
+;; expand region
+;; key chords
+;; dumb jump
+
+;; contextual grep
+;; deadgrep improvements
+
+(map! :n "z z" 'evil-avy-goto-char-timer)
+(map! :n "z s" 'swiper)
+(map! :n "z r" 'er/expand-region)
+(map! :n "z f" 'projectile-find-file-other-window)
+
+(after! company-lsp
+  (setq company-lsp-cache-candidates 'auto)
+  (add-to-list 'company-lsp-filter-candidates '(mspyls . t))
+  (defun company-lsp--on-completion (response prefix)
+    "Handle completion RESPONSE.
+PREFIX is a string of the prefix when the completion is requested.
+Return a list of strings as the completion candidates."
+    (let* ((incomplete (and (hash-table-p response) (gethash "isIncomplete" response)))
+           (items (cond ((hash-table-p response) (gethash "items" response))
+                        ((sequencep response) response)))
+           (candidates (mapcar (lambda (item)
+                                 (company-lsp--make-candidate item prefix))
+                               (lsp--sort-completions items)))
+           (server-id (lsp--client-server-id (lsp--workspace-client lsp--cur-workspace)))
+           (should-filter (or (eq company-lsp-cache-candidates 'auto) ; change from t to 'auto
+                              (and (null company-lsp-cache-candidates)
+                                   (company-lsp--get-config company-lsp-filter-candidates server-id)))))
+      (when (null company-lsp--completion-cache)
+        (add-hook 'company-completion-cancelled-hook #'company-lsp--cleanup-cache nil t)
+        (add-hook 'company-completion-finished-hook #'company-lsp--cleanup-cache nil t))
+      (when (eq company-lsp-cache-candidates 'auto)
+        ;; Only cache candidates on auto mode. If it's t company caches the
+        ;; candidates for us.
+        (company-lsp--cache-put prefix (company-lsp--cache-item-new candidates incomplete)))
+      (if should-filter
+          (company-lsp--filter-candidates candidates prefix)
+        candidates))))
 
 (use-package! super-save
   :config (progn
-            (setq super-save-auto-save-when-idle t)
-            (setq super-save-idle-duration 8)
+            (setq super-save-auto-save-when-idle nil)
             (super-save-mode +1)))
