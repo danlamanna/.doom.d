56f3bbda9481fa0c4dfed80e42b86deff3a7cd72
Dan LaManna <dan.lamanna@kitware.com>
2021-02-16 11:40:42 -0500

    Add packages

--- packages.el
+++ packages.el
@@ -2,3 +2,12 @@
 ;;; .doom.d/packages.el
 
 (package! super-save)
+(package! dap-mode)
+(package! aggressive-indent)
+(package! magit-delta)
+(package! color-identifiers-mode)
+(package! smooth-scrolling)
+(package! visual-regexp-steroids)
+(package! activity-watch-mode)
+(package! edit-server)
+(package! string-inflection)
